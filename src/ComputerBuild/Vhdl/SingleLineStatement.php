60dd5473b29eeca4a3a199a423c6990044fc11cb
Luis Cordova <cordoval@gmail.com>
2012-04-24 20:55:10 -0500

    port singlelinestatement

---
+++ src/ComputerBuild/Vhdl/SingleLineStatement.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ComputerBuild\Vhdl;
+
+class SingleLineStatement extends Statement
+{
+    public function generate($out, $indent)
+    {
+        for ($i = 0; $i <= $indent; $i++) {
+            $out->print("  ");
+        }
+        $out->print(parent::line());
+        $out->print("\n");
+    }
+}
